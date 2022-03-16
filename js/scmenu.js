305dca9f14b334e887c5381e7b5f406106e8679d
Gottbow <garyprado45@gmail.com>
2022-03-15 20:54:47 -0500

    ver

---
+++ js/scmenu.js
@@ -0,0 +1,5 @@
+const btnMenu = document.querySelector('#menu'),
+    menuContent = document.querySelector('.menu-content');
+btnMenu.addEventListener('click', () => {
+    menuContent.classList.toggle('menu-active')
+})
